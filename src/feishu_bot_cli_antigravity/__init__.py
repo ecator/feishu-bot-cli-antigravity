06fe26b6e08d0dbbb7f1d8b83247c1bf30f1477d
Martin <qule520@126.com>
2026-09-12 20:49:06 +0800

    style: 统一相对导入风格

--- src/feishu_bot_cli_antigravity/__init__.py
+++ src/feishu_bot_cli_antigravity/__init__.py
@@ -7,10 +7,10 @@ try:
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 
-from feishu_bot_cli_antigravity.agent import AgentSessionManager
-from feishu_bot_cli_antigravity.channel import FeishuBotChannel
-from feishu_bot_cli_antigravity.config import Config, get_config
-from feishu_bot_cli_antigravity.utils import create_media_from_bytes, setup_logging
+from .agent import AgentSessionManager
+from .channel import FeishuBotChannel
+from .config import Config, get_config
+from .utils import create_media_from_bytes, setup_logging
 
 __all__ = [
     "AgentSessionManager",
